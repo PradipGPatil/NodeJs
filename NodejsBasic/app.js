eaea69473a7aadb12cb7af314495a5078733de1d
pradip <pradippatil1543@gmail.com>
2025-06-08 17:33:01 +0530

    added node module

--- NodejsBasic/app.js
+++ NodejsBasic/app.js
@@ -4,3 +4,8 @@ const routes=require('./routes')
 const server=http.createServer(routes.handler);
 
 server.listen(3000);
+
+// we want to start server automatiocally whern we made change
+// we can use nodemon to do that
+// npm install nodemon --save-dev 
+//make change in package.json file
